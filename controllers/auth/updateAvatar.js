7f2bcc231a2ad46401551a6235009bfea17e7da1
SyrdiiDima <dimasyrdij@icloud.com>
2022-10-21 16:31:40 +0300

    complete

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,15 +1,20 @@
 const { unlink } = require("fs");
 const fs = require("fs/promises");
 const path = require("path");
-const jimp = require("jimp");
+
 const { User } = require("../../models/user");
+const Jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
   try {
-    const { _id } = req.user;
-    const { path: tempUpload, originalname } = req.file;
+    const newPhoto = await Jimp.read(tempUpload);
+    newPhoto.resize(250, 250).write(tempUpload);
+
     const extention = originalname.split(".").pop();
+
     const filename = `${_id}.${extention}`;
 
     const resultUpload = path.join(avatarsDir, filename);
@@ -22,7 +27,7 @@ const updateAvatar = async (req, res) => {
       avatarURL,
     });
   } catch (error) {
-    await fs, unlink(req.file.path);
+    await fs.unlink(req.file.path);
     throw error;
   }
 };
